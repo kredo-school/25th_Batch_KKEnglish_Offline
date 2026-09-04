2af4a82e4dfaadfc6fb7b5f4658146ea6f0745df
otakiosamu <o.otaki.o.728@gmail.com>
2026-09-04 12:37:51 +0800

    Variable material and seeder adding

--- app/Http/Controllers/Student/ReservationController.php
+++ app/Http/Controllers/Student/ReservationController.php
@@ -4,19 +4,43 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Teacher;
+use App\Models\Material;
 use Illuminate\Contracts\View\View;
 
 class ReservationController extends Controller
 {
     public function index(): View
     {
+        /**
+         * Teacher一覧
+         *
+         * user
+         * materials
+         *
+         * も一緒に取得
+         */
         $teachers = Teacher::query()
-            ->with('user')
+            ->with(['user', 'materials'])
             ->get();
 
+         /*
+         * Material一覧
+         */
+
+        $materials = Material::query()
+            ->orderBy('material_id')
+            ->get();
+
+        /*
+         * Bladeへ
+         * $teachers
+         * $materials
+         *
+         * を渡す
+         */
         return view(
             'students.reservations.index',
-            compact('teachers')
+            compact('teachers','materials')
         );
     }
 }
--- app/Models/Material.php
+++ app/Models/Material.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Teacher;
+use App\Models\Reservation;
 class Material extends Model
 {
     protected $table = 'materials';
@@ -21,8 +24,20 @@ class Material extends Model
         'status',
     ];
     public function reservations(): HasMany
-{
-    return $this->hasMany(Reservation::class, 'material_id', 'material_id');
-}
+    {
+        return $this->hasMany(Reservation::class, 'material_id', 'material_id');
+    }
+
+    public function teachers(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Teacher::class,
+            'teacher_materials',
+            'material_id',
+            'teacher_id',
+            'material_id',
+            'id'
+        )->withTimestamps();
+    }
 
 }
