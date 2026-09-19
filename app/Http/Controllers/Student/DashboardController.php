d525c24e2dc58ee10551568ad9672c257c1dd133
otakiosamu <o.otaki.o.728@gmail.com>
2026-09-19 16:24:19 +0800

    Dashboard refine

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Announcement;
 use App\Models\Reservation;
 
 class DashboardController extends Controller
@@ -48,13 +49,117 @@ class DashboardController extends Controller
 
             // 時間順
             ->orderBy('start_at')
+            ->get();
+
+        $nextLessons = Reservation::query()
+
+            // ログイン中の生徒だけ
+            ->where('student_id', $student->id)
+
+            // 終わっていない予約だけ
+            ->where('end_at', '>', now())
+
+            // 有効な予約だけ
+            ->whereHas('status', function ($query) {
+                $query->whereIn('status_code', [
+                    'pending',
+                    'confirmed',
+                ]);
+            })
+
+            // 関連データも一緒に取得
+            ->with([
+                'teacher.user',
+                'material',
+                'status',
+            ])
+
+            // 時間順
+            ->orderBy('start_at')
 
             ->get();
 
+        $nextLesson = $nextLessons->first();
+
+        /*
+         * Student向けお知らせ
+         */
+        $announcements = Announcement::query()
+            ->whereIn(
+                'target',
+                [
+                    'all',
+                    'students',
+                ]
+            )
+            ->latest()
+            ->get();
+
+
+        // 可能であれば
+        /*
+        * カレンダー表示用予約
+        */
+        $calendarReservations = Reservation::query()
+
+            ->where(
+                'student_id',
+                $student->id
+            )
+
+            ->whereHas(
+                'status',
+                function ($query) {
+                    $query->whereIn(
+                        'status_code',
+                        [
+                            'pending',
+                            'confirmed',
+                        ]
+                    );
+                }
+            )
+            ->with([
+                'teacher.user',
+                'material',
+                'status',
+            ])
+            ->get();
+
+
+        /*
+         * FullCalendar用データ
+        */
+        $calendarEvents = $calendarReservations
+            ->map(
+                function ($reservation) {
+                    return [
+                        'title' =>
+                        $reservation
+                            ->teacher
+                            ->user
+                            ->first_name,
+
+                        'start' =>
+                        $reservation->start_at,
+
+                        'end' =>
+                        $reservation->end_at,
+                    ];
+                }
+            );
+
+
         return view(
             'students.dashboard',
-            compact('student','todayLessons')
+            compact(
+                'student',
+                'todayLessons',
+                'announcements',
+                'nextLesson',
+                'calendarReservations',
+                'calendarEvents'
+            )
         );
     }
-
 }
